96717eafa770eab1bcb79629e385e259aabca936
Saleh-Wolf <salehfarid1997@gmail.com>
2026-03-05 02:34:42 +0200

    updating my work

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+          public function index()
+    {
+        return Post::with(['category','user'])->paginate(5);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::apiResource('categories', CategoryController::class);
+Route::get('/posts', [PostController::class, 'index']);
